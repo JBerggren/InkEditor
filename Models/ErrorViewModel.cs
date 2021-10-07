edc560c86d473f386de1917494eafa269be7404e
JBerggren <jonatan.berggren@gmail.com>
2021-10-07 15:56:22 +0200

    Inital cm

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InkBackend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
